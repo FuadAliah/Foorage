50ba214f01b95583d061315f59d654d2a09985cb
FuadAliah <eye.design17@yahoo.com>
2020-05-05 00:41:05 +0300

    Updates

---
+++ precache-manifest.62b88bc1e18692bc6eee089914c56bef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f489407947f206751eb3",
+    "url": "/Foorage/static/css/main.eb594d5e.chunk.css"
+  },
+  {
+    "revision": "f489407947f206751eb3",
+    "url": "/Foorage/static/js/main.622d25d0.chunk.js"
+  },
+  {
+    "revision": "e1217c5cedf069dcc2ab",
+    "url": "/Foorage/static/js/runtime~main.388f1678.js"
+  },
+  {
+    "revision": "0c5124b167463c68355c",
+    "url": "/Foorage/static/css/2.b1d0e94d.chunk.css"
+  },
+  {
+    "revision": "0c5124b167463c68355c",
+    "url": "/Foorage/static/js/2.4f19d89c.chunk.js"
+  },
+  {
+    "revision": "6d7f017379b05179b5cebf3cd706dd17",
+    "url": "/Foorage/static/media/logo.6d7f0173.svg"
+  },
+  {
+    "revision": "a3bc46b64dbc6b17775c89dc9f99e87b",
+    "url": "/Foorage/static/media/home_section_1.a3bc46b6.jpg"
+  },
+  {
+    "revision": "ec815161dce42cb9e1515a5ef3fb2c11",
+    "url": "/Foorage/static/media/banner_bg_desktop.ec815161.jpg"
+  },
+  {
+    "revision": "6cb5bbc6e5e0d53745c7b0a8904f6c76",
+    "url": "/Foorage/static/media/bg_call_section.6cb5bbc6.jpg"
+  },
+  {
+    "revision": "f833cdc60d6b61d0dab47e558e313736",
+    "url": "/Foorage/index.html"
+  }
+];
