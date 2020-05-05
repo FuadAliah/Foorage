2a0909d21a116da652810d5fdd3837f076e961cc
FuadAliah <eye.design17@yahoo.com>
2020-05-05 21:00:09 +0300

    ad

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { HashRouter as Router } from 'react-router-dom'
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Router>
+  , document.getElementById('root')
 );
 
 serviceWorker.unregister();
